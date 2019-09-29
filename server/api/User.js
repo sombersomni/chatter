dfcd9ca9b5651c7c2aaaf1e30fb192c362c74ec7
somberSomni <kartunehustla@gmail.com>
2019-09-29 01:46:35 -0500

    starting front end

--- server/api/User.js
+++ server/api/User.js
@@ -1,6 +1,6 @@
 const { ObjectID } = require('mongodb');
 let users;
-
+const options = { projection: { username: 1, email: 1 } };
 class User {
     static injectDB(db) {
         if (db) {
@@ -9,18 +9,20 @@ class User {
     }
     static async getUser(username) {
         try {
-            return await users.findOne({ username }, { username: 1, email: 1 });
-        } catch(err) {
+            return await users.findOne({ username }, options);
+        } catch (err) {
             console.log(err.message);
             return null;
         }
     }
     static async getUsers(username) {
         try {
-            return await users.findOne({}, { username: 1, email: 1 }).toArray();
-        } catch(err) {
+            return await users.find({}, options).toArray();
+        } catch (err) {
             console.log(err.message);
             return [];
         }
     }
 }
+
+module.exports = User;
--- server/test/user.test.js
+++ server/test/user.test.js
@@ -1,11 +1,23 @@
 const axios = require('axios');
+const user = {
+    email: 'test@test.com',
+    id: "5d904d9c69674322f05c52ba",
+    username: 'test',
+}
 
 describe('User', function() {
     it('can get all users in db', function() {
         return axios.get('http://localhost:3000/user')
             .then(res => {
                 expect(res.status).toBe(200);
-                expect()
+                //expect(res.data).toContainEqual(user);
+            })
+    })
+    it('can get a single user', function() {
+        return axios.get(`http://localhost:3000/user/${user.username}`)
+            .then(res => {
+                expect(res.status).toBe(200);
+                expect(res.data.username).toEqual(user.username);
             })
     })
 })
